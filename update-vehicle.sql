2cc265ff00e37fd85d8867460ada522938eae7e7
Shwetha K M <shmurali98@gmail.com>
2018-11-30 13:04:17 +0530

    Add files via upload
    Update vehicle

---
+++ update-vehicle.sql
@@ -0,0 +1,8 @@
+use dbmspro;
+
+update vehicle set model_name="Palisade" where vehicle_id="V01";
+update vehicle set model_name="Elantra" where vehicle_id="V02";
+update vehicle set model_name="Veloster" where vehicle_id="V03";
+update vehicle set model_name="i40 Sedan" where vehicle_id="V04";
+update vehicle set model_name="Nexo" where vehicle_id="V05";
+
